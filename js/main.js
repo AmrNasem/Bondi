a21a77486c1a79e035331257be2341e458b06787
Amr <amr.nasem51@gmail.com>
2023-07-23 19:33:09 +0300

    Created the services section

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const navLinks = document.querySelectorAll("#nav-id .nav-link");
 
 navLinks.forEach((navLink) => {
   navLink.addEventListener("click", () => {
+    if (navLink.getAttribute("href") === "#home") window.scrollTo(0, 0);
     navLinks.forEach((link) => link.classList.remove("active"));
     navLink.classList.add("active");
   });
